e7a00d0caf2a065fbe9181ff8bb81aac53a8dbe9
Григорий <grinzya@yandex.ru>
2021-03-09 16:07:13 +0500

    Добавлен DbContext

---
+++ Book_Library_NETCore_st_proj/Models/BookLibContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Book_Library_NETCore_st_proj.Models
+{
+    public class BookLibContext : DbContext
+    {
+        public DbSet<Authors> Authors { get; set; }
+        public DbSet<Books> Books { get; set; }
+        public DbSet<Genres> Genres { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<User> User { get; set; }
+
+        public BookLibContext(DbContextOptions<BookLibContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
