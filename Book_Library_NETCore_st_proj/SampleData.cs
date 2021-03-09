017bc639af40d0575f99e28fa35ee8dad5a085de
Григорий <grinzya@yandex.ru>
2021-03-10 02:20:52 +0500

    Добавлен пример тестовых данных

---
+++ Book_Library_NETCore_st_proj/SampleData.cs
@@ -0,0 +1,24 @@
+﻿using Book_Library_NETCore_st_proj.Models;
+using System.Linq;
+
+namespace Book_Library_NETCore_st_proj
+{
+    public static class SampleData
+    {
+        public static void Initialize(BookLibContext context)
+        {
+            if(!context.Books.Any())
+            {
+                context.Books.AddRange(
+                    new Books
+                    {
+                        AuthorID = 1,
+                        BookName = "Приключения Арсена Люпена",
+                        GenreID = 1,
+                        StockRemains = 20
+                    }
+                    );
+            }
+        }
+    }
+}
