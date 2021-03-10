fe9cb9029e3a395f2c77280000bcaf0b0c751b3a
Григорий <grinzya@yandex.ru>
2021-03-10 13:50:48 +0500

    Добавлен инициализатор базы данных при запуске

--- Book_Library_NETCore_st_proj/Program.cs
+++ Book_Library_NETCore_st_proj/Program.cs
@@ -1,5 +1,7 @@
+using Book_Library_NETCore_st_proj.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,23 @@ namespace Book_Library_NETCore_st_proj
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+
+            var host =  CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<BookLibContext>();
+                    SampleData.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
